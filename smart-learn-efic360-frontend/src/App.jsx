44a9059fc25dce99b9ed523a6cd83e91c47cdbea
SareesMHM <sareeshaniffa@gmail.com>
2025-05-22 02:05:57 +0530

    fuc

--- smart-learn-efic360-frontend/src/App.jsx
+++ smart-learn-efic360-frontend/src/App.jsx
@@ -25,7 +25,7 @@ import MentorList from './pages/MentorList';
 import TeacherDashboard from './Teachers/pages/TeacherDashboard';
 import AdminDashboard from './Admin/pages/AdminDashboard';
 import UserManagement from './Admin/pages/UserManagement';
-
+import ParentDashboard from './Parent/pages/ParentDashboard';
 import AccessLogViewer from './Admin/pages/AccessLogViewer';
 
 import CourseManager from './Admin/pages/CourseManager';
@@ -34,7 +34,10 @@ import GradeClassManager from './Admin/pages/GradeClassManager';
 import ContentManager from './Admin/pages/ContentManager';
 import Feedback from './Admin/pages/Feedback';
 import ReportAnalytics from './Admin/pages/ReportAnalytics';
+import VerificationCenter from './Admin/pages/VerificationCenter';
 import OfflineLearning from './pages/OfflineLearning';
+import ForgotPassword from './pages/ForgotPassword';
+
 // src/index.js or src/App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -46,36 +49,29 @@ function App() {
   return (
        <Routes>
          <Route path="/" element={<Home />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
           <Route path="/ProfileEdit" element={<ProfileEdit />} />
           <Route path="/ProfileView" element={<ProfileView />} />
-          <Route path="/TeacherDashboard" element={<TeacherDashboard />} />
-          
+          <Route path="/FeedbackList" element={<FeedbackList />} />
+          <Route path="/Teacher/TeacherDashboard" element={<TeacherDashboard />} />
+          <Route path="/Parent/ParentDashboard" element={<ParentDashboard />} />
+          <Route path="/Admin/VerificationCenter" element={<VerificationCenter />} />
           <Route path="/OfflineLearning" element={<OfflineLearning />} />
           <Route path="/NotificationsPage" element={<NotificationsPage />} />
-          <Route path="/Feedback" element={<Feedback />} />
+          <Route path="/Admin/Feedback" element={<Feedback />} />
           <Route path="/MentorList" element={<MentorList />} />
-         <Route path="/ReportAnalytics" element={<ReportAnalytics />} />
+         <Route path="/Admin/ReportAnalytics" element={<ReportAnalytics />} />
          <Route path="/AccessLogViewer" element={<AccessLogViewer />} />
-
-
-        
          <Route path="/PerformanceAnalyticsPage" element={<PerformanceAnalyticsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
         <Route path="/StudentDashboard" element={<StudentDashboard />} />
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-        <Route path="/AdminRegisterUserForm" element={<AdminRegisterUserForm />} />
-        <Route path="/ContentManager" element={<ContentManager />} />
-        <Route path="/GradeClassManager" element={<GradeClassManager />} />
-        <Route path="/GradeFeedback" element={<GradeFeedback />} />
-
-        
-        <Route path="/UserManagement" element={<UserManagement />} />
-        <Route path="/CourseManager" element={<CourseManager />} />
-
-        
-
-        
+        <Route path="/Admin/AdminDashboard" element={<AdminDashboard />} />
+        <Route path="/Admin/AdminRegisterUserForm" element={<AdminRegisterUserForm />} />
+        <Route path="/Admin/ContentManager" element={<ContentManager />} />
+        <Route path="/Admin/GradeClassManager" element={<GradeClassManager />} />
+        <Route path="/Admin/UserManagement" element={<UserManagement />} />
+        <Route path="/Admin/CourseManager" element={<CourseManager />} />
         <Route path="/AttendancePage" element={<AttendancePage />} />
         <Route path="/AdaptiveQuizPage" element={<AdaptiveQuizPage />} />
         <Route path="/BadgeSystemPage" element={<BadgeSystemPage />} />
